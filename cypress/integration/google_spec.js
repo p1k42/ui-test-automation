85930ea485e10e57b7296423cb80cdf719fa8636
T41-Ch1 <dekabitasp@gmail.com>
2022-02-25 01:27:03 +0900

    update cypress

--- cypress/integration/google_spec.js
+++ cypress/integration/google_spec.js
@@ -1,6 +1,6 @@
 describe("Google Test", () => {
   it('Shows Google', () => {
-    cy.visit('https://www.http://typingx0.net/sushida/')
+    cy.visit('http://typingx0.net/sushida/')
 
     cy.title().should("include", "寿司打")
   })
